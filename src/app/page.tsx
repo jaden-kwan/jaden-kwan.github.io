711f361ddf005df0e82b0e00557ac125cc71ddf2
jaden-kwan <105980272+jaden-kwan@users.noreply.github.com>
2024-10-05 00:49:02 -0400

    page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <section className="py 24">
-      <div className="container">
-        <h1 className="text-3x1 font-bold">Portfolio</h1>
+      <div className="container max-w-3xl">
+        <h1 className="text-3xl font-bold">Portfolio</h1>
       </div>
     </section>
   )
